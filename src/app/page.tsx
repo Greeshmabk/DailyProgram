a27a60961153e9aae1e2bc474a11d82a762691d5
Greeshma BK <greeshmameengoth@gmail.com>
2024-12-12 19:16:24 +0100

    changing tasks variable to use useState hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,28 @@
+"use client";
+import { useState } from "react";
 import Tasks from "./components/Tasks";
 
-
 export default function Home() {
-  let tasksList = [
+  const [tasksList, setTaskList] = useState([
     {
       id: 1,
       title: "1st Task",
-      dueDate: new Date("2024-12-03")
+      dueDate: new Date("2024-12-03"),
     },
     {
       id: 2,
       title: "Go Shopping",
-      dueDate: new Date("2024-12-03")
+      dueDate: new Date("2024-12-03"),
     },
     {
       id: 3,
       title: "complete task",
-      dueDate: new Date("2024-12-03")
+      dueDate: new Date("2024-12-03"),
     },
-  ];
+  ]);
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Day Planning </h1>
+    <div className="bg-red-100" style={{ textAlign: "center" }}>
+      <h1 className="text-5xl font-bold text-green-500 p-6">Day Planning </h1>
       <Tasks tasks={tasksList}></Tasks>
     </div>
   );
