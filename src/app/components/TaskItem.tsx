5da6f0680e36f8f0f0125da2b47bdd6bbea00d5e
Greeshma BK <greeshmameengoth@gmail.com>
2024-12-30 14:07:10 +0100

    added Task field for adding new task

--- src/app/components/TaskItem.tsx
+++ src/app/components/TaskItem.tsx
@@ -1,6 +1,6 @@
 const TaskItem = ({ taskDetails }) => {
   return (
-    <span className="py-3 text-2xl ">
+    <span className="py-3 text-2xl">
       <span className="px-2">{taskDetails.title}</span>
     </span>
   );
