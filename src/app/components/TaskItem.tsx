495c9a519d7297c42a2020a5ac6e05d916f57c1b
Greeshma BK <greeshmameengoth@gmail.com>
2025-01-11 20:56:25 +0100

    code changes for expiry date

--- src/app/components/TaskItem.tsx
+++ src/app/components/TaskItem.tsx
@@ -1,8 +1,11 @@
 const TaskItem = ({ taskDetails }) => {
   return (
-    <span className="py-3 text-2xl">
-      <span className="px-2">{taskDetails.title}</span>
-    </span>
+    <div className={`py-3 text-2xl ${
+      taskDetails.remainingDays > 0 ? "text-blue-500" : "text-red-500"
+    }`}>
+      <div className="px-2 "><span className="font-bold">Task:</span>{taskDetails.title}</div>
+      <div className="px-2"><span className="font-bold">Reamaining Days:</span> {taskDetails.remainingDays} days</div>
+    </div>
   );
 };
 
